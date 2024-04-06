d6029ebcafc53c0b93ec1952176b29045f06caac
kyle-p-myers <kmyers2438@gmail.com>
2024-04-06 12:18:47 -0600

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import OfferTwo from "@/public/components/OfferTwo";
 import * as React from "react";
 import RootLayout from "./layout";
 import OfferThree from "@/public/components/OfferThree";
+import PricingOne from "@/public/components/PricingOne";
 
 
 export default function Home() {
@@ -27,7 +28,16 @@ export default function Home() {
       <div className="grid col-start-3 w-96 h-96 bg-transparent border border-[#ADF77F] rounded-lg shadow-2xl">
         <OfferThree />
       </div> 
-    </div>
+      </div>
+      <div className="mt-[10%] text-5xl">
+      Pricing
+      </div>
+      <div className="grid grid-cols-3 w-11/12 gap-2 mt-[5%]">
+      <div className="grid col-start-1 w-[450px] h-[560px] border border-[#ADF77F] bg-transparent rounded-lg shadow-2xl">
+        <PricingOne/>
+      </div> 
+      </div>
+    
    
     </main>
 
